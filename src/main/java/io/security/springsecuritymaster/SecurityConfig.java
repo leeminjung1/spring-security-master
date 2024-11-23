c411464f5a87feef287ed5fb43ffc209b59131a7
leeminjung1 <vipminjung@gmail.com>
2024-11-23 17:08:04 +0900

    폼 인증 - formLogin()

--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,27 +1,64 @@
 package io.security.springsecuritymaster;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 @EnableWebSecurity
 @Configuration
+@Slf4j
 public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
 			.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-			.formLogin(Customizer.withDefaults())
+			.formLogin(form -> form
+				.loginPage("/loginPage")
+				.loginProcessingUrl("/loginProc")
+				.defaultSuccessUrl("/", true)
+				.failureUrl("/failed")
+				.usernameParameter("userId")
+				.passwordParameter("passwd")
+				.successHandler(new AuthenticationSuccessHandler() {
+					@Override
+					public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+						Authentication authentication) throws IOException, ServletException {
+						log.info("authentication: " + authentication);
+						response.sendRedirect("/home");
+					}
+				})
+				.failureHandler(new AuthenticationFailureHandler() {
+					@Override
+					public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+						AuthenticationException exception) throws IOException, ServletException {
+						log.info("exception: " + exception.getMessage());
+						response.sendRedirect("/login");
+					}
+				})
+				.permitAll()
+			)
 			.build();
 	}
+
 
 	@Bean
 	public UserDetailsService userDetailsService() {
@@ -29,12 +66,6 @@ public class SecurityConfig {
 			.password("{noop}1111")
 			.roles("USER")
 			.build();
-
-		UserDetails user2 = User.withUsername("user2")
-			.password("{noop}1111")
-			.roles("USER")
-			.build();
-
-		return new InMemoryUserDetailsManager(user, user2);
+		return new InMemoryUserDetailsManager(user);
 	}
 }
